c2ec670f952d911c49be1af17e535786c26d370a
Wladek <wladek.airo@gmail.com>
2019-03-05 11:57:10 +0300

    update db name

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'ukweli'),
+            'database' => env('DB_DATABASE', 'ukweli_db'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'toor'),
             'unix_socket' => env('DB_SOCKET', ''),
