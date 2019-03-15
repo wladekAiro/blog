7e28abdd4d2720767a9c300d1f07fe8834b6fb74
Wladek <wladek.airo@gmail.com>
2019-03-15 08:39:39 +0300

    Use soft delete on models

--- app/Image.php
+++ app/Image.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Image extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'name' , 'link'
     ];
