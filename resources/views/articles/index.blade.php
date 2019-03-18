36f7e6d801424733cfcedeaec6ca07c89fa16417
Wladek <wladek.airo@gmail.com>
2019-03-18 10:11:38 +0300

    Disable vue

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="col-md-10">
+        Test
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,18 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Route::get('/articles', 'ArticlesController@index')->name('articles');
 
 Auth::routes();
 
