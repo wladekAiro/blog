372430871f57aa15fb6ab5c9395d29e8c7240fb0
Wladek <wladek.airo@gmail.com>
2019-04-10 12:25:42 +0300

    Add slug to article view model

--- app/ViewModels/ArticleViewModel.php
+++ app/ViewModels/ArticleViewModel.php
@@ -11,21 +11,26 @@ namespace App\ViewModels;
 
 class ArticleViewModel
 {
+    public $slug;
     public $title;
     public $body;
+    public $articleStatus;
     public $imageUrl;
     public $writerName;
     public $editorName;
     public $writerId;
     public $editorId;
 
-    function __construct($title , $body , $imageUrl , $writerName , $editorName , $writerId , $editorId) {
-        $this -> title = $title;
-        $this -> body = $body;
-        $this -> imageUrl = $imageUrl;
-        $this -> writerName = $writerName;
-        $this -> editorName = $editorName;
-        $this -> writerId = $writerId;
-        $this -> editorId = $editorId;
+    function __construct($slug, $title, $body, $articleStatus, $imageUrl, $writerName, $editorName, $writerId, $editorId)
+    {
+        $this->slug = $slug;
+        $this->title = $title;
+        $this->body = $body;
+        $this->articleStatus = $articleStatus;
+        $this->imageUrl = $imageUrl;
+        $this->writerName = $writerName;
+        $this->editorName = $editorName;
+        $this->writerId = $writerId;
+        $this->editorId = $editorId;
     }
 }
